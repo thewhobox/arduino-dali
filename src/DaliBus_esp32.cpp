d7b74c1a859e791c871f4a5cd0f5acebc685da89
Mike <45664417+thewhobox@users.noreply.github.com>
2025-01-22 11:20:13 +0100

    simple5

--- src/DaliBus_esp32.cpp
+++ src/DaliBus_esp32.cpp
@@ -24,16 +24,17 @@ static rmt_transmit_config_t transmit_config;
 
 static bool dali_rmt_rx_callback(rmt_channel_handle_t channel, const rmt_rx_done_event_data_t *edata, void *user_data)
 {
-    printf("dali_rmt_rx_callback\r\n");
+    daliClass->flag = !daliClass->flag;
+    //printf("dali_rmt_rx_callback\r\n");
     BaseType_t high_task_wakeup = pdFALSE;
     // QueueHandle_t receive_queue = (QueueHandle_t)user_data;
     // // send the received RMT symbols to the parser task
     // xQueueSendFromISR(receive_queue, edata, &high_task_wakeup);
 
-    printf("edata:                           %p\r\n", edata);
-    printf("user_data:                       %p\r\n", user_data);
-    DaliBusClass *daliClass = (DaliBusClass *)user_data;
-    printf("daliClass:                       %p\r\n", daliClass);
+    // printf("edata:                           %p\r\n", edata);
+    // printf("user_data:                       %p\r\n", user_data);
+    DaliBusClass *daliClass = static_cast<DaliBusClass*>(user_data);
+    // printf("daliClass:                       %p\r\n", daliClass);
     gpio_intr_enable(daliClass->getRxPin());
     return high_task_wakeup == pdTRUE;
 }
@@ -82,13 +83,12 @@ static void IRAM_ATTR onDALIFrameStart(void* arg)
     // printf("onDALIFrameStart\r\n");
     // printf("arg:                             %p\r\n", arg);
     DaliBusClass *daliClass = static_cast<DaliBusClass*>(arg);
-    daliClass->flag = !daliClass->flag;
     // printf("daliClass:                       %p\r\n", daliClass);
     // printf("daliClass uID:                   %.4X\r\n", daliClass->uniqueId);
-    // esp_err_t resp = gpio_intr_disable(daliClass->getRxPin());
+    gpio_intr_disable(daliClass->getRxPin());
     // printf("gpio_intr_disable:               %d (%s)\n", resp, esp_err_to_name(resp));
 
-    // resp = rmt_receive(daliClass->getRxHandle(), daliClass->rawSymbols, sizeof(daliClass->rawSymbols), &(daliClass->dali_rxReceiveConfig));
+    rmt_receive(daliClass->getRxHandle(), daliClass->rawSymbols, sizeof(daliClass->rawSymbols), &(daliClass->dali_rxReceiveConfig));
     // printf("rmt_receive:                     %d (%s)\n", resp, esp_err_to_name(resp));
 }
 
