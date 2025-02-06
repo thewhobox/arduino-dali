e33928a82f22becfa6cfeeaf60056d79e24b28d0
Mike <45664417+thewhobox@users.noreply.github.com>
2025-02-06 14:12:24 +0100

    simple26

--- src/DaliBus_esp32.cpp
+++ src/DaliBus_esp32.cpp
@@ -251,7 +251,7 @@ esp_err_t DaliBusClass::decode_symbols(rmt_rx_done_event_data_t *rx_data, uint32
 
     for (size_t i = 1; i < rx_data->num_symbols; i++) {
         printf("Duration0: %d, Level0: %d\n", rx_data->received_symbols[i].duration0, rx_data->received_symbols[i].level0);
-        printf("Duration1: %d, Level1: %d\n", rx_data->received_symbols[i].duration0, rx_data->received_symbols[i].level0);
+        printf("Duration1: %d, Level1: %d\n", rx_data->received_symbols[i].duration1, rx_data->received_symbols[i].level1);
     }
 
     // TODO dont depend on receiving only 8bits
